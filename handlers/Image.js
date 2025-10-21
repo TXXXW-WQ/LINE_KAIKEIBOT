1fca9837637a928351c6d28398e732330450233d
TXXXW-WQ <tokiwaryu001@gmail.com>
2025-10-21 21:55:32 +0900

    feat:画像取得用エンドポイント記述

--- handlers/Image.js
+++ handlers/Image.js
@@ -2,6 +2,9 @@
  * webhookによって受信したcontentIdをもとにAPIでコンテンツを取得？
  * @param {string}
  */
-function Image (contentId) {
+function Image(inputDate) {
+  const contentJson = inputDate.json();
   
+  Logger.log(contentJson);
+  const url = "https://api-data.line.me/v2/bot/message/{messageId}/content";
 }
