2b7f6126e8caf656c7b577450087b90d8ce5d52a
TXXXW-WQ <tokiwaryu001@gmail.com>
2025-10-27 19:45:40 +0900

    fix:Image関数を修正

--- handlers/Image.js
+++ handlers/Image.js
@@ -2,17 +2,17 @@
  * webhookによって受信したcontentIdをもとにAPIでコンテンツを取得？
  * @param {string}
  */
-function Image(messageId) {
-  const GEMINI_API_KEY =  PropertiesService.getScriptProperties().getProperty('GEMINI_API_KEY');
+function Image(userText) {
+  // const GEMINI_API_KEY =  PropertiesService.getScriptProperties().getProperty('GEMINI_API_KEY');
   const image = `https://api-data.line.me/v2/bot/message/${messageId}/content`;
   try {
-    if (!API_KEY) {
-      throw new Error("APIキーが設定されていません。スクリプトプロパティを確認してください。");
-    }
+    // if (!API_KEY) {
+    //   throw new Error("APIキーが設定されていません。スクリプトプロパティを確認してください。");
+    // }
     this.session = {
       ...this.session,
       step: 8,
-      receipt_image: image
+      objective: userText,
     }
     return 
   } catch {
