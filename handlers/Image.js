74132bb87b094c676a7d52eedb84de53fd52191e
TXXXW-WQ <tokiwaryu001@gmail.com>
2025-10-28 22:36:09 +0900

    feat:ValidateImageにtry文追加

--- handlers/Image.js
+++ handlers/Image.js
@@ -4,7 +4,6 @@
  */
 function Image(userText) {
   // const GEMINI_API_KEY =  PropertiesService.getScriptProperties().getProperty('GEMINI_API_KEY');
-  const image = `https://api-data.line.me/v2/bot/message/${messageId}/content`;
   try {
     // if (!API_KEY) {
     //   throw new Error("APIキーが設定されていません。スクリプトプロパティを確認してください。");
--- handlers/ValidateImage.js
+++ handlers/ValidateImage.js
@@ -1,3 +1,7 @@
 function ValidateImage(messageId) {
-  
+  try {
+    const image = `https://api-data.line.me/v2/bot/message/${messageId}/content`;
+  } catch (e) {
+    Logger.log('画像の取得中にエラーが発生しました: ' + e.toString());
+  }
 }
