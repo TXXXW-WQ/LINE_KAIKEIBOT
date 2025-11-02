a23aab63d6fc0033638dac3c4d39b21dbf997d4d
TXXXW-WQ <tokiwaryu001@gmail.com>
2025-11-02 22:33:59 +0900

    feat:payload追加

--- handlers/ValidateImage.js
+++ handlers/ValidateImage.js
@@ -1,11 +1,29 @@
 function ValidateImage(messageId) {
   try {
+    const replyToken = this.replyToken
+    if (!replyToken) {
+      Logger.log('replyTokenがありません。');
+    }
+    const CHANNEL_ACCESS_TOKEN = PropertiesService.getScriptProperties().getProperty('LINE_CHANNEL_ACCESS_TOKEN');
     const url = `https://api-data.line.me/v2/bot/message/${messageId}/content`;
-    // Logger.log('画像の取得に成功しました。')
-    const response = UrlFetchApp.fetch(url);
+
+    const payload = {
+      replyToken: replyToken
+    }
+    const options = {
+      'method': 'post',
+      'headers': {
+        'Authorization': 'Bearer ' + CHANNEL_ACCESS_TOKEN,
+        'Content-Type': 'application/json'
+      },
+      'payload': JSON.stringify(payload),
+      'muteHttpExceptions': true
+    };
+
+    const response = UrlFetchApp.fetch(url, options);
     this.session = {
       ...this.session,
-      step:9,
+      step: 9,
       receipt_image: image
     }
 
