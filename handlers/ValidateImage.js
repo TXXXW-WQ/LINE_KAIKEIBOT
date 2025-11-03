792b177a867c2ed611b4328bf0dd2b67f73bf53f
TXXXW-WQ <tokiwaryu001@gmail.com>
2025-11-03 21:59:13 +0900

    fix:画像取得リクエスト修正

--- handlers/ValidateImage.js
+++ handlers/ValidateImage.js
@@ -11,7 +11,7 @@ function ValidateImage(messageId) {
       replyToken: replyToken
     }
     const options = {
-      'method': 'post',
+      'method': 'get',
       'headers': {
         'Authorization': 'Bearer ' + CHANNEL_ACCESS_TOKEN,
         'Content-Type': 'application/json'
@@ -21,11 +21,20 @@ function ValidateImage(messageId) {
     };
 
     const response = UrlFetchApp.fetch(url, options);
-    this.session = {
-      ...this.session,
-      step: 9,
-      receipt_image: image
+    if (response.getResponseCode() === 200) {
+      const imageBlob = response.getBlob();
+      Logger.log('画像データを取得しました。ファイル名: ' + imageBlob.getName() + ', サイズ: ' + imageBlob.getBytes().length + 'バイト');
+
+      this.session = {
+        ...this.session,
+        step: 9,
+        receipt_image: image
+      }
+    } else {
+      Logger.log("画像取得失敗")
+      return
     }
+
 
   } catch (e) {
     Logger.log('画像の取得中にエラーが発生しました: ' + e.toString());
